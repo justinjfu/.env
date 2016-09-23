264e4af0dd27d0a1c8209a2eee250ba968a88f97
Justin Fu <justinjfu@gmail.com>
2016-09-22 22:16:34 -0700

    Add swp dir

--- setup/vim_setup.sh
+++ setup/vim_setup.sh
@@ -4,6 +4,7 @@ echo 'Wiping .vimrc!'
 echo "source $ENV_DIR/.vimrc" > ~/.vimrc
 
 mkdir -p ~/.vim
+mkdir -p ~/.vim/swp
 mkdir -p ~/.vim/bundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
